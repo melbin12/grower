a926683088121babe76f8076fac2e80f7f21e7fc
Melbin12 <melbinmp2023b@mca.ajce.in>
2022-08-16 21:56:00 +0530

    The grower

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">The Grower</a>
+      </header>
+      <main>
+        List product
+      </main>
+    </div>
+  );
+}
+
+export default App;
